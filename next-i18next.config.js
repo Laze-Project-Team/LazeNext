a9a4ae73131d4434ccc398e716e99ecaf078b70c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2021-12-17 23:05:56 +0900

    i18next

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,9 @@
-const path = require('path');
-
+// const path = require('path');
 
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
   },
-  localePath: path.resolve('./src/locales/'),
+  localePath: './src/locales/',
 };
--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 const { i18n } = require('./next-i18next.config');
-const { nextI18NextRewrites } = require('next-i18next/rewrites');
 
 const localeSubpaths = {};
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   i18n,
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
   },
