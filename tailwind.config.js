3db0b3c59189019df4bc726ed5bcd7e197b7b270
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2021-12-17 21:45:20 +0900

    Tailwind CSSv3.0に対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
