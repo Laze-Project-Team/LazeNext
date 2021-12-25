3d1b4a12e7bc217f350521dfd85c4df3d346e620
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2021-12-25 14:20:26 +0900

    resizer.css読み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import '@/styles/contextmenu.css';
 import '@/styles/editor-scrollable.css';
 import 'monaco-editor/min/vs/editor/editor.main.css';
 import '@/styles/indexlist.css';
+import '@/styles/resizer.css';
 
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
