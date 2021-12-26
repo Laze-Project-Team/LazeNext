55e71254fab27a5d1f0e16bac41a2e35e102e794
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2021-12-26 10:59:59 +0900

    format

--- src/components/model/WorkBench/Console/ConsoleSeparator.tsx
+++ src/components/model/WorkBench/Console/ConsoleSeparator.tsx
@@ -1,9 +1,11 @@
 import type { VFC } from 'react';
 
-export const ConsoleSeparator: VFC = () => {return (
-  <>
-    <div className="p-2">
-      <div className="bg-[#777] h-[1px]" />
-    </div>
-  </>
-)};
+export const ConsoleSeparator: VFC = () => {
+  return (
+    <>
+      <div className="p-2">
+        <div className="bg-[#777] h-[1px]" />
+      </div>
+    </>
+  );
+};
