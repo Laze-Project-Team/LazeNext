f9113f169cc287844279daf8a77b92555bf32f5b
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2021-12-26 11:04:05 +0900

    ディレクトリ定数

--- src/const/dir.ts
+++ src/const/dir.ts
@@ -4,3 +4,5 @@ export const ROOT_DIR = search > 0 ? __dirname.slice(0, search - 1) : __dirname;
 export const SAMPLE_DIR = ROOT_DIR + '/data/samples';
 export const CACHE_DIR = ROOT_DIR + '/cache';
 export const COMMON_DIR = ROOT_DIR + '/data/common';
+export const LANG_DIR = ROOT_DIR + '/data/lang';
+export const PARSER_DIR = ROOT_DIR + '/data/parser';
