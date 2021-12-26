a885ae925df0461cc4108d091e3413e7e96c70de
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2021-12-26 11:08:05 +0900

    format

--- src/components/model/MonacoEditor/Loading.tsx
+++ src/components/model/MonacoEditor/Loading.tsx
@@ -1,10 +1,12 @@
 import { Spin } from 'antd';
 import type { VFC } from 'react';
 
-export const Loading: VFC = () => {return (
-  <>
-    <div className="w-full h-full flex justify-center items-center">
-      <Spin size="large" />
-    </div>
-  </>
-)};
+export const Loading: VFC = () => {
+  return (
+    <>
+      <div className="w-full h-full flex justify-center items-center">
+        <Spin size="large" />
+      </div>
+    </>
+  );
+};
