4c3c1ad41e9b45bd07e54a681d6466b813a190ed
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-03 00:24:54 +0900

    コンパイルボタンのスタイル修正

--- src/components/model/EditorButtons/EditorButton/EditorButton.tsx
+++ src/components/model/EditorButtons/EditorButton/EditorButton.tsx
@@ -12,7 +12,7 @@ export const EditorButton: FC<EditorButtonProps> = ({ name, onClick, Icon, disab
     <>
       <button
         type="button"
-        className="inline-flex relative items-center space-x-1 h-full px-3 border-b-2 transition-all ease-linear border-transparent hover:border-primary-400 hover:text-primary-400 dark:hover:border-primary-100 dark:hover:text-primary-100"
+        className="inline-flex relative items-center space-x-1 h-full px-3 border-b-2 transition-all ease-linear border-transparent hover:border-primary-400 hover:text-primary-400 dark:hover:border-primary-100 dark:hover:text-primary-100 disabled:hover:!border-transparent disabled:!text-gray-500 disabled:dark:!text-gray-400"
         onClick={onClick}
         disabled={disabled ?? false}
       >
