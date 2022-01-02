8446a992984ce70def3338b5d5ef085ce8f2875c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-02 20:08:36 +0900

    スピナーを自作のものに差し替え

--- src/components/model/MonacoEditor/Loading.tsx
+++ src/components/model/MonacoEditor/Loading.tsx
@@ -1,11 +1,15 @@
-import { Spin } from 'antd';
 import type { VFC } from 'react';
+import { VscLoading } from 'react-icons/vsc';
+
+import { Spin } from '@/components/ui/Spin';
 
 export const Loading: VFC = () => {
   return (
     <>
       <div className="w-full h-full flex justify-center items-center">
-        <Spin size="large" />
+        <Spin className="text-6xl">
+          <VscLoading />
+        </Spin>
       </div>
     </>
   );
