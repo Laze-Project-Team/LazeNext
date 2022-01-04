e209abf793f7f6a0cc51872ff571e3dd9883dcd5
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-04 22:36:12 +0900

    SelectableListのライトモード時のスタイル修正

--- src/components/ui/SelectableList.tsx
+++ src/components/ui/SelectableList.tsx
@@ -32,7 +32,7 @@ export const SelectableList: VFC<SelectableListProps> = ({ id, items, selectedIt
               />
               <label
                 htmlFor={`${id}-${key}`}
-                className="inline-block px-4 py-1 w-full h-full hover:bg-black/10 dark:hover:bg-white/10 transition-colors duration-200 rounded-sm peer-checked:bg-black/10 dark:peer-checked:bg-white/10 peer-checked:hover:bg-black/[.15] peer-checked:dark:hover:bg-white/20 cursor-pointer"
+                className="inline-block px-4 py-1 w-full h-full hover:bg-black/5 dark:hover:bg-white/10 transition-colors duration-200 rounded-sm peer-checked:bg-black/5 dark:peer-checked:bg-white/10 peer-checked:hover:bg-black/10 peer-checked:dark:hover:bg-white/20 cursor-pointer"
               >
                 {items[key as keyof typeof items]}
               </label>
