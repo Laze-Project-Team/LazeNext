057864b45d66f37319fd0045b5d8e4d656681b4e
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-06 22:28:06 +0900

    metaタグ追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,12 @@ class Document extends NextDocument {
     return (
       <Html>
         <Head>
+          {/* 基本設定 */}
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+          {/* ファビコン */}
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
