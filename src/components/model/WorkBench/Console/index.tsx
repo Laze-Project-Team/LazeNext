25ed7098df4d56f6b17bf191e08aa029461ccc0a
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-10 00:16:53 +0900

    SelectableListのスタイル修正（editor-scrollableを非module化）

--- src/components/model/WorkBench/Console/index.tsx
+++ src/components/model/WorkBench/Console/index.tsx
@@ -7,8 +7,6 @@ import { ConsoleLogs } from '@/components/model/WorkBench/Console/ConsoleLogs';
 import { ConsolePanelListItem } from '@/components/model/WorkBench/Console/ConsolePanelListItem';
 import type { consoleState } from '@/features/redux/console';
 import type { RootState } from '@/features/redux/root';
-import { cx } from '@/features/utils/cx';
-import styles from '@/styles/editor-scrollable.module.css';
 
 const UnconnectedConsole: VFC<Omit<consoleState, 'scrolled'>> = ({ console, active }) => {
   const [t] = useTranslation('editor');
@@ -17,16 +15,10 @@ const UnconnectedConsole: VFC<Omit<consoleState, 'scrolled'>> = ({ console, acti
 
   return (
     <div className="h-full flex">
-      <div className={cx('flex-1 h-full overflow-y-scroll', styles.scrollable, styles.normal)} ref={consoleRef}>
+      <div className={'flex-1 h-full overflow-y-scroll editor-scrollable scrollable-thin'} ref={consoleRef}>
         <ConsoleLogs consoleRef={consoleRef} />
       </div>
-      <div
-        className={cx(
-          'w-28 ml-1 border-l-[1px] border-gray-300 dark:border-gray-700 editor-scrollable overflow-x-hidden overflow-y-auto',
-          styles.scrollable,
-          styles.thin
-        )}
-      >
+      <div className="w-28 ml-1 border-l-[1px] border-gray-300 dark:border-gray-700 editor-scrollable scrollable-thin overflow-x-hidden overflow-y-auto">
         {Object.keys(console).map((key) => {
           return (
             <ConsolePanelListItem
