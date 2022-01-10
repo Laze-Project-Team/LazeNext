7c72d8bf98f446a48032a818672b260fcaa6e8c2
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-10 12:38:50 +0900

    カスタムサーバーでSSL実装

---
+++ server/index.ts
@@ -0,0 +1,85 @@
+/* eslint-disable no-console */
+import compression from 'compression';
+import express from 'express';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+import next from 'next';
+
+const CERT_DIR = './certs';
+const PORT = parseInt(process.env.PORT ?? '', 10) || 3000;
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+type uncheckedCredentials = {
+  key: string | false;
+  cert: string | false;
+};
+
+type checkedCredentials = {
+  key: string;
+  cert: string;
+};
+
+const credentials: uncheckedCredentials = {
+  key: fs.existsSync(`${CERT_DIR}/key.pem`) && fs.readFileSync(`${CERT_DIR}/key.pem`, { encoding: 'utf-8' }),
+  cert: fs.existsSync(`${CERT_DIR}/cert.pem`) && fs.readFileSync(`${CERT_DIR}/cert.pem`, { encoding: 'utf-8' }),
+};
+
+app.prepare().then(() => {
+  if (dev) {
+    const server = express();
+    server.use(compression({ level: 9, memLevel: 9 }));
+
+    server.all('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(PORT, () => {
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
+  } else {
+    if (!checkCert(credentials)) {
+      throw new Error('Missing SSL certificate');
+    }
+
+    if (PORT !== 443) {
+      throw new Error('SSL port must be 443');
+    }
+
+    // http
+    {
+      const server = express();
+
+      server.all('*', (req, res) => {
+        // 全てhttpsにリダイレクト
+        res.redirect(`https://${req.hostname}${req.url}`);
+      });
+
+      http.createServer(server).listen(80);
+    }
+    // https
+    {
+      const server = express();
+      server.use(compression({ level: 9, memLevel: 9 }));
+
+      server.all('*', (req, res) => {
+        return handle(req, res);
+      });
+
+      https.createServer(credentials, server).listen(PORT, () => {
+        console.log(`> Ready on https://localhost:${PORT} (SSL)`);
+      });
+    }
+  }
+});
+
+const checkCert = (credentials: uncheckedCredentials): credentials is checkedCredentials => {
+  if (!credentials.key || !credentials.cert) {
+    return false;
+  }
+
+  return true;
+};
