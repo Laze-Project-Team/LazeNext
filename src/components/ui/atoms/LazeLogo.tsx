7eab933f4e11239a53c0c8904532d4990f0de3d2
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-11 21:30:50 +0900

    画像最適化が行われるように

--- src/components/ui/atoms/LazeLogo.tsx
+++ src/components/ui/atoms/LazeLogo.tsx
@@ -3,6 +3,10 @@ import { useTranslation } from 'next-i18next';
 import type { VFC } from 'react';
 
 import { cx } from '@/features/utils/cx';
+import logo from '@/img/logo/logo.png';
+import logo_caption from '@/img/logo/logo_caption.png';
+import logo_caption_gray from '@/img/logo/logo_caption_gray.png';
+import logo_gray from '@/img/logo/logo_gray.png';
 
 type logoPotion = 'logo' | 'logo_gray' | 'logo_caption' | 'logo_caption_gray';
 
@@ -12,13 +16,20 @@ type LazeLogoProps = {
   className?: string;
 };
 
+const images: Record<logoPotion, StaticImageData> = {
+  logo,
+  logo_caption,
+  logo_gray,
+  logo_caption_gray,
+};
+
 export const LazeLogo: VFC<LazeLogoProps> = ({ size, option = 'logo', className }) => {
   const [t] = useTranslation('common');
 
   return (
     <>
       <div className={cx('inline-flex select-none pointer-events-none', className)}>
-        <Image src={`/img/logo/${option}.png`} alt={t('lazelogo')} width={size} height={size} />
+        <Image src={images[option]} alt={t('lazelogo')} width={size} height={size} />
       </div>
     </>
   );
