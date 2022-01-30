a0567f30bc0edc79628e497675b5d436441f30d5
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-30 11:42:47 +0900

    🚧 アカウント登録ページ作成(WIP)

---
+++ src/pages/signup.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { IndexLayout } from '@/components/layout/IndexLayout';
+import { H1 } from '@/components/ui/IndexLayout';
+
+const Privacy: NextPage = () => {
+  const { locale } = useRouter();
+  const [t] = useTranslation('signup');
+  const title = `${t('title')} | Laze`;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+
+        <meta content={t('description')} name="description" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={t('description')} />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={`https://laze.ddns.net/${locale + '/' ?? ''}signup`} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:locale" content={locale ?? 'en'} />
+      </Head>
+
+      <IndexLayout>
+        <H1>{t('title')}</H1>
+      </IndexLayout>
+    </>
+  );
+};
+
+type contextType = {
+  locale: string;
+};
+export const getStaticProps = async (context: contextType) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(context.locale, ['common', 'signup', 'layout'])),
+    },
+  };
+};
+
+export default Privacy;
