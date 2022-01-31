13540d6f4d752517d3576dd3dc28c0402e78d7f1
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-31 17:46:16 +0900

    🚧 アカウントシステム整備(WIP)

--- src/components/ui/SignInScreen.tsx
+++ src/components/ui/SignInScreen.tsx
@@ -1,20 +1,17 @@
+import { Button } from 'antd';
 import { useRouter } from 'next/router';
 import type { VFC } from 'react';
-import { useState } from 'react';
 import { useEffect } from 'react';
 
+import { useAuthContext } from '@/components/model/Context/AuthContext';
 import { auth } from '@/features/firebase';
-
-import { useAuthContext } from '../model/Context/AuthContext';
 
 export const SignInScreen: VFC = () => {
   const router = useRouter();
   const { locale } = router;
-  const [isShown, setIsShown] = useState(false);
   const { user } = useAuthContext();
 
   useEffect(() => {
-    setIsShown(true);
     auth.languageCode = router.locale ?? 'en';
   }, [router.locale]);
 
@@ -24,9 +21,9 @@ export const SignInScreen: VFC = () => {
     }
   }, [locale, router, user]);
 
-  if (isShown) {
-    return <div className="my-16"></div>;
-  }
-
-  return <div></div>;
+  return (
+    <div>
+      <Button></Button>
+    </div>
+  );
 };
