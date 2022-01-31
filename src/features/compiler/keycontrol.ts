65b16e8e49acb23d5779ba6f425cf7f40089b258
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-01-31 17:34:23 +0900

    🐛 エディタ以外からエディタに遷移した際にkeyCheckが上手く動かないバグを修正

--- src/features/compiler/keycontrol.ts
+++ src/features/compiler/keycontrol.ts
@@ -1,44 +1,42 @@
 export const keyControlInitialize = (): void => {
   let lastDownTarget: EventTarget | null;
 
-  window.addEventListener('load', () => {
-    const canvas = <HTMLCanvasElement>document.getElementById('output-canvas');
-    document.addEventListener(
-      'mousedown',
-      (e) => {
-        window.laze.props.variables.keyControl.mousePressed = true;
-        lastDownTarget = e.target;
-      },
-      false
-    );
-    document.addEventListener(
-      'mouseup',
-      (e) => {
-        if (e.target === canvas) {
-          window.laze.props.variables.keyControl.mousePressed = false;
-        }
-      },
-      false
-    );
-    document.addEventListener(
-      'keydown',
-      (e) => {
-        if (lastDownTarget === canvas) {
-          window.laze.props.variables.keyControl.pressedKeys[e.keyCode] = true;
-        }
-      },
-      false
-    );
-    document.addEventListener(
-      'keyup',
-      (e) => {
-        if (lastDownTarget === canvas) {
-          window.laze.props.variables.keyControl.pressedKeys[e.keyCode] = false;
-        }
-      },
-      false
-    );
-  });
+  const canvas = <HTMLCanvasElement>document.getElementById('output-canvas');
+  document.addEventListener(
+    'mousedown',
+    (e) => {
+      window.laze.props.variables.keyControl.mousePressed = true;
+      lastDownTarget = e.target;
+    },
+    false
+  );
+  document.addEventListener(
+    'mouseup',
+    (e) => {
+      if (e.target === canvas) {
+        window.laze.props.variables.keyControl.mousePressed = false;
+      }
+    },
+    false
+  );
+  document.addEventListener(
+    'keydown',
+    (e) => {
+      if (lastDownTarget === canvas) {
+        window.laze.props.variables.keyControl.pressedKeys[e.keyCode] = true;
+      }
+    },
+    false
+  );
+  document.addEventListener(
+    'keyup',
+    (e) => {
+      if (lastDownTarget === canvas) {
+        window.laze.props.variables.keyControl.pressedKeys[e.keyCode] = false;
+      }
+    },
+    false
+  );
 };
 
 export const initialKeyControl = {
