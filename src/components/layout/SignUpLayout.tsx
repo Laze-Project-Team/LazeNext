414207c843a886b57b89a3e31b09116177b32385
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-02-02 22:11:27 +0900

    🚧 エラーを修正

--- src/components/layout/SignUpLayout.tsx
+++ src/components/layout/SignUpLayout.tsx
@@ -1,4 +1,5 @@
 import { Divider } from 'antd';
+import { useTranslation } from 'next-i18next';
 import type { FC, ReactNode } from 'react';
 
 type SignUpLayoutProps = {
@@ -8,6 +9,8 @@ type SignUpLayoutProps = {
 };
 
 export const SignUpLayout: FC<SignUpLayoutProps> = ({ title, authWith, form }) => {
+  const [t] = useTranslation('signup');
+
   return (
     <div className="mx-auto max-w-[30rem] px-4 pt-8">
       <h1 className="mb-8 text-center text-2xl">{title}</h1>
