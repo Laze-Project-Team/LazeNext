03066b2fe8afd8e672440dd822dd4e7e93b24e91
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-07-13 15:17:47 +0900

    🎨 styling header

--- src/components/model/Learn/Header.tsx
+++ src/components/model/Learn/Header.tsx
@@ -1,9 +1,16 @@
 import type { FC } from 'react';
 
+import { LazeLogo } from '@/components/ui/atoms/LazeLogo';
+
 export const Header: FC = () => {
   return (
     <>
-      <header>ヘッダーだよ</header>
+      <header className="flex px-4 py-2 shadow-sm">
+        <div className="inline-flex items-center space-x-1">
+          <LazeLogo size={32} />
+          <span className="select-none text-2xl text-laze-primary">Laze</span>
+        </div>
+      </header>
     </>
   );
 };
