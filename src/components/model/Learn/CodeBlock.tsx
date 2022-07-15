68745b82a4b882fe7a3d08e42fd589efadd8ed7c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-07-15 16:03:00 +0900

    🎨 styling codeblock

--- src/components/model/Learn/CodeBlock.tsx
+++ src/components/model/Learn/CodeBlock.tsx
@@ -5,12 +5,11 @@ import { Editor } from './Editor';
 
 export const Pre: Components['pre'] = ({ children }) => {
   const code = children[0] as ReactElement;
-
-  console.log(code);
+  const codeValue = code.props.children as string[];
 
   const className = code.props?.className as string | undefined;
   if (className && className.split(' ').includes('language-laze')) {
-    const value = code.props.children.join('');
+    const value = codeValue.join('');
     const [initialValue, placeholder] = value.split('---\n');
 
     return <Editor initialValue={initialValue} placeholder={placeholder} />;
@@ -18,7 +17,21 @@ export const Pre: Components['pre'] = ({ children }) => {
 
   return (
     <>
-      <p></p>
+      <pre className="my-8 flex rounded-sm bg-gray-100 font-editor">
+        <code className="bg-gray-200 px-2 py-2">
+          {codeValue
+            .join('')
+            .split('\n')
+            .map((_, i) => {
+              return i + 1;
+            })
+            .filter((_, i, arr) => {
+              return i < arr.length - 1;
+            })
+            .join('\n')}
+        </code>
+        <code className="px-2 py-2 font-editor">{codeValue}</code>
+      </pre>
     </>
   );
 };
