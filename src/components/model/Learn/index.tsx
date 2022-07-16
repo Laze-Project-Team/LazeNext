1eeba468923c2871d10e30329bf9661b8c22fed8
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-07-16 17:01:40 +0900

    🎨 a little styling

--- src/components/model/Learn/index.tsx
+++ src/components/model/Learn/index.tsx
@@ -39,7 +39,7 @@ export const LearnPage: FC = () => {
         <Header />
 
         <div className="flex flex-1">
-          <nav className="w-60 space-y-1 overflow-y-auto p-2">
+          <nav className="w-60 space-y-1 overflow-y-auto border-r p-2">
             <p className="px-2 text-lg font-bold text-gray-700">{t('table_of_contents')}</p>
             <Markdown
               components={{
@@ -51,7 +51,7 @@ export const LearnPage: FC = () => {
               {content}
             </Markdown>
           </nav>
-          <div className="h-full min-w-0 flex-1 overflow-y-auto px-4" ref={mainRef}>
+          <div className="h-full min-w-0 flex-1 overflow-y-auto px-8" ref={mainRef}>
             <main className="pt-8">
               <Markdown
                 components={{
